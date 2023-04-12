61850d3944bc74f11d383218db419ddbda73b4b8
Hongye-Chen <122873068+Hongye-Chen@users.noreply.github.com>
2023-04-12 06:24:35 -0700

    Add files via upload

---
+++ EEG_Preprocessing.py
@@ -0,0 +1,214 @@
+import numpy as np
+import os
+import mne
+from mne.preprocessing import EOGRegression
+import pandas as pd
+
+# Define file path
+file_path = r"Y:\Projects\2023_Scholte_FMG1441\Data"
+SubMatrix_path = r"C:\Users\15202\OneDrive\C_\University of Amsterdam\Intern\CAVA_project"
+#Specify the subject name
+subject_name = "sub_5"
+#Read the Image list
+Sub_Images_list = pd.read_csv(os.path.join(SubMatrix_path, "Sub_matrix",subject_name + "_randomized_matrix_702.csv"), header = None)
+Sub_Images_list = Sub_Images_list.values.tolist()
+Sub_Images_list = [i for sub in Sub_Images_list for i in sub]   #Flatten the list
+#Read the Image dictionary
+All_Images_df = pd.read_csv(os.path.join(SubMatrix_path, "EventsID_Dictionary.csv"), header = None)
+All_Images_dict = dict(zip(All_Images_df[0], All_Images_df[1]))
+# All_Images_list = pd.read_csv(os.path.join(SubMatrix_path, "All Images' filenames.csv"), header = None)
+# All_Images_list = All_Images_list.values.tolist()
+# All_Images_list = [i for sub in All_Images_list for i in sub]   #Flatten the list
+# values_list = list(range(1, len(All_Images_list)+1))
+# All_Images_dict = dict(zip(All_Images_list, values_list))
+# df = pd.DataFrame(All_Images_dict.items())
+# df.to_csv(os.path.join(SubMatrix_path, "EventsID_Dictionary.csv"), header = None, index = False)
+# Read raw data
+raw = mne.io.read_raw_bdf(os.path.join(file_path, subject_name, subject_name + ".bdf"))
+
+#Crop data to save memory if needed
+# raw.crop(tmax=800)
+
+# Drop irrelevant channels
+raw.drop_channels(['EXG7', 'EXG8', 'GSR1', 'GSR2', 'Erg1', 'Erg2', 'Resp', 'Plet', 'Temp'])
+# raw.rename_channels({'left-ref':'EXG7', 'right-ref':'EXG8'})
+
+# Reset reference channels type from eeg to eog
+raw.set_channel_types({'left': 'eog', 'right': 'eog', 'above': 'eog', 'below': 'eog'})
+print(raw.info)
+
+# Load data
+raw.pick(['eeg', 'eog', 'stim']).load_data()
+
+#Rereference to earlobes/mastoids (average of left and right)
+#Check eeg config file the name for mastoids, and then rename them
+raw.set_eeg_reference(ref_channels=['left-ref', 'right-ref'])
+
+#What is our baseline?
+Baseline = (-0.099609375, 0)
+
+# Is it okay we use the common location map? We have different placement of P5 and P6.
+biosemi64_montage = mne.channels.make_standard_montage(kind = 'biosemi64')
+EEG_64channels = biosemi64_montage.ch_names
+eog_channels = ['left', 'right', 'above', 'below']
+# biosemi64_montage.plot()
+raw.set_montage(biosemi64_montage, on_missing = 'ignore')
+events = mne.find_events(raw)
+
+# Define graphs parameters
+plot_kwargs = dict(ylim=dict(eeg=(-15, 15), eog=(-15, 15)))
+
+
+####################################################################################################################
+# Filters:
+#     Butterworth Zero Phase Filters
+#     Low Cutoff: 0.1 Hz, Time constant 1.5915s, 12 dB/oct ???????????
+#     High Cutoff: 30.0 Hz, 24 dB/oct ????????????
+#     Notch Filter: 50 Hz and 60 Hz
+raw.filter(l_freq=0.1, h_freq=30, method='iir', iir_params=None,picks = 'all')
+raw.filter(l_freq=0.1, h_freq=30, method='fir', phase = 'zero', picks = 'all')
+# raw.filter(l_freq=0.1, h_freq=30, method='fir', phase = 'zero', filter_length = '1s', picks = 'all')
+# alueError: The requested filter length 1025 is too short for the requested 0.05 Hz transition band, which requires 33793 samples
+raw.notch_filter(freqs = (50, 60), picks = 'all')
+# plot_kwargs = dict(ylim=dict(eeg=(-10, 10), eog=(-5, 15)))
+raw.plot_psd(fmax = 35)
+####################################################################################################################
+
+
+####################################################################################################################
+# artifact removal
+    # Minimal allowed amplitude: -250.00 µV, Max: 250.00 µV
+    # MNE can reject epochs based on maximum peak-to-peak signal amplitude (PTP),
+    # i.e. the absolute difference between the lowest and the highest signal value.
+    # How can I remove signals based on singel peak but not differences between peaks?
+
+    # Annotating continuous data
+    # onsets = events[:, 0] / raw.info['sfreq'] - 0.5
+    # durations = [1] * len(events)
+    # descriptions = ['bad_event'] * len(events)
+    # event_annotation = mne.Annotations(onsets, durations, descriptions, orig_time=raw.info['meas_date'])
+    # raw.set_annotations(event_annotation)
+####################################################################################################################
+
+
+####################################################################################################################
+# Segmentation
+# o Segment size and position relative to reference markers:
+# o Start: -100 ms ????, End: 500.00 ms?????, Length: 600 ms?????
+# How long the tmin and tmax should be? Images are contiguous to the gray screen
+reject_criteria = dict(eeg=50e-6)
+flat_criteria = dict(eeg = 50e-8)
+# autoreject()
+# from autoreject import get_rejection_threshold
+# reject_criteria = get_rejection_threshold(epochs)
+# epochs = mne.Epochs(raw, events, tmin=-0.1, tmax=0.4, baseline = Baseline,
+#                     reject = reject_criteria, flat = flat_criteria)
+# events = mne.find_events(raw, shortest_event = 1)
+events_len = len(events)
+CurIndex_event = 0
+converted_events = []
+for i in range(0,events_len):
+    converted_events.append(events[i, :])
+    if events[i, 2] > 250:
+        converted_events[i][2] = 5000 + events[i, 2]
+        continue
+    elif 1 <= events[i, 2] <= 250:
+        CurImage = Sub_Images_list[CurIndex_event]
+        if CurImage.startswith('Stimuli\\Targets\\'):
+            CurImage = CurImage[16:]
+        elif CurImage.startswith('Stimuli\\'):
+            CurImage = CurImage[8:]
+        converted_events[i][2] = All_Images_dict[CurImage]
+        CurIndex_event += 1
+converted_events = np.array(converted_events)
+epochs = mne.Epochs(raw, events, tmin=-0.1, tmax=0.4, baseline = Baseline,
+                    reject = reject_criteria, flat = flat_criteria, picks = EEG_64channels+eog_channels)
+epochs.load_data()
+stimuli_list = [str(i) for i in list(range(1,4708))]
+stimuli_epochs = epochs[stimuli_list]
+stimuli_epochs.load_data()
+
+# epochs.plot()
+# stimuli_epochs.plot()
+stimuli_epochs_plot_before = stimuli_epochs.average('all').plot()
+stimuli_epochs_plot_before.set_size_inches(18, 9)
+stimuli_epochs_plot_before.savefig(os.path.join(SubMatrix_path,subject_name + "_stimuli_epochs_before(with reject criteria).png"))
+
+epochs_NoRestriction = mne.Epochs(raw, events, tmin=-0.1, tmax=0.4, picks = EEG_64channels+eog_channels)
+epochs_NoRestriction.load_data()
+stimuli_epochs_NoRestriction = epochs_NoRestriction[stimuli_list]
+stimuli_epochs_NoRestriction.load_data()
+stimuli_epochs_plot_before_NoRestriction = stimuli_epochs_NoRestriction.average('all').plot()
+stimuli_epochs_plot_before_NoRestriction.set_size_inches(18, 9)
+stimuli_epochs_plot_before_NoRestriction.savefig(os.path.join(SubMatrix_path,subject_name + "_stimuli_epochs_before(4800 events).png"))
+####################################################################################################################
+
+
+####################################################################################################################
+# Ocular correction (Gratton & Coles)
+# https://mne.tools/dev/auto_tutorials/preprocessing/35_artifact_correction_regression.html
+# https://mne.tools/dev/auto_tutorials/preprocessing/40_artifact_correction_ica.html
+# eog_epochs = mne.preprocessing.create_eog_epochs(raw, baseline = Baseline
+model_plain = EOGRegression(picks='eeg', picks_artifact='eog').fit(stimuli_epochs)
+stimuli_epochs_clean_plain = model_plain.apply(stimuli_epochs)
+stimuli_epochs_clean_plain.apply_baseline()
+# model_plain.plot()
+stimuli_epochs_eogout_plot = stimuli_epochs_clean_plain.average().plot(**plot_kwargs)
+stimuli_epochs_eogout_plot.set_size_inches(18, 9)
+stimuli_epochs_eogout_plot.savefig(os.path.join(SubMatrix_path,subject_name + "_stimuli_epochs_after_OC(with reject criteria).png"))
+
+model_plain_NoRestriction = EOGRegression(picks='eeg', picks_artifact='eog').fit(stimuli_epochs_NoRestriction)
+stimuli_epochs_clean_plain_NoRestriction = model_plain_NoRestriction.apply(stimuli_epochs_NoRestriction)
+stimuli_epochs_clean_plain_NoRestriction.apply_baseline()
+stimuli_epochs_eogout_plot_NoRestriction= stimuli_epochs_clean_plain_NoRestriction.average().plot(**plot_kwargs)
+stimuli_epochs_eogout_plot_NoRestriction.set_size_inches(18, 9)
+stimuli_epochs_eogout_plot_NoRestriction.savefig(os.path.join(SubMatrix_path,subject_name + "_stimuli_epochs_after_OC(4800 events).png"))
+
+
+
+    # This function (mne.preprocessing.create_eog_epochs) will:
+    #
+    # Filter the EOG data channel.
+    #
+    # Find the peaks of eyeblinks in the EOG data using mne.preprocessing.find_eog_events().
+    #
+    # Create Epochs around the eyeblinks.
+# eog_epochs.plot_image(combine='mean')
+####################################################################################################################
+
+
+####################################################################################################################
+# CSD transformation
+#     o Order of splines: 4
+#     o Maximal degree of Legendre polynomials: 10
+#     o Approximation parameter Lambda: 1.000000e-005
+# stimuli_epochs.pick_channels(EEG_64channels)
+stimuli_epochs_csd = mne.preprocessing.compute_current_source_density(stimuli_epochs_clean_plain, lambda2=1e-5, stiffness=4, n_legendre_terms=10)
+# stimuli_epochs_csd.plot()
+# stimuli_epochs_csd.plot_psd(fmax = 35)
+evoked_plot = stimuli_epochs_csd.average().plot(**plot_kwargs)
+evoked_plot.set_size_inches(18, 9)
+evoked_plot.savefig(os.path.join(SubMatrix_path,subject_name + "_stimuli_epochs_after_OC&CSD(with reject criteria).png"))
+
+stimuli_epochs_csd_NoRestriction = mne.preprocessing.compute_current_source_density(stimuli_epochs_clean_plain_NoRestriction, lambda2=1e-5, stiffness=4, n_legendre_terms=10)
+evoked_plot_NoRestriction = stimuli_epochs_csd_NoRestriction.average().plot(**plot_kwargs)
+evoked_plot_NoRestriction.set_size_inches(18, 9)
+evoked_plot_NoRestriction.savefig(os.path.join(SubMatrix_path,subject_name + "_stimuli_epochs_after_OC&CSD(4800 events).png"))
+
+####################################################################################################################
+
+
+####################################################################################################################
+# Make a plot of averaged epoched response, and save the plot
+# Evoked response?
+# evoked = stimuli_epochs_csd.average()
+# evoked_plot = evoked.plot()
+# evoked_plot.set_size_inches(18, 9)
+# evoked_plot.savefig(os.path.join(SubMatrix_path,subject_name + "_evoked_plot.png"))
+# evoked.save(os.path.join(file_path, (subject_name + "_ave.fif")))
+# stimuli_evoked = stimuli_epochs.average()
+# stimuli_evoked.plot()
+
+# Save the preprocessed data
+# stimuli_epochs.save(os.path.join(file_path, (subject_name + "_epo.fif")))
+####################################################################################################################
