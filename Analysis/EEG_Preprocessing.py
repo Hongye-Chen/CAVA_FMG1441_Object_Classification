0521781874e36fb853648985218d3faf78b147b6
Hongye-Chen <122873068+Hongye-Chen@users.noreply.github.com>
2023-05-05 09:40:38 +0200

    Add files via upload

---
+++ Analysis/EEG_Preprocessing.py
@@ -0,0 +1,216 @@
+import numpy as np
+import os
+import mne
+from mne.preprocessing import EOGRegression
+import pandas as pd
+from autoreject import get_rejection_threshold
+
+
+# Define file path
+file_path = r"Y:\Projects\2023_Scholte_FMG1441\Data"
+SubMatrix_path = r"C:\Users\15202\OneDrive\C_\University of Amsterdam\Intern\CAVA_project"
+# Specify the subject name
+subject_name = input("Enther the subject number (in form of sub_x): ")
+# Read the Image list
+Sub_Images_list = pd.read_csv(os.path.join(SubMatrix_path, "Sub_matrix", subject_name + "_randomized_matrix_702.csv"), header=None)
+Sub_Images_list = Sub_Images_list.values.tolist()
+Sub_Images_list = [i for sub in Sub_Images_list for i in sub]   #Flatten the list
+# Read the Image dictionary
+All_Images_df = pd.read_csv(os.path.join(SubMatrix_path, "EventsID_Dictionary.csv"), header = None)
+All_Images_dict = dict(zip(All_Images_df[0], All_Images_df[1]))
+# All_Images_list = pd.read_csv(os.path.join(SubMatrix_path, "All Images' filenames.csv"), header = None)
+# All_Images_list = All_Images_list.values.tolist()
+# All_Images_list = [i for sub in All_Images_list for i in sub]   #Flatten the list
+# values_list = list(range(1, len(All_Images_list)+1))
+# All_Images_dict = dict(zip(All_Images_list, values_list))
+# df = pd.DataFrame(All_Images_dict.items())
+# df.to_csv(os.path.join(SubMatrix_path, "EventsID_Dictionary.csv"), header = None, index = False)
+# Read raw data
+raw = mne.io.read_raw_bdf(os.path.join(file_path, subject_name, subject_name + ".bdf"))
+
+# Crop data to save memory if needed
+# raw.crop(tmax=800)
+
+# Drop irrelevant channels
+raw.drop_channels(['EXG7', 'EXG8', 'GSR1', 'GSR2', 'Erg1', 'Erg2', 'Resp', 'Plet', 'Temp'])
+# raw.rename_channels({'left-ref':'EXG7', 'right-ref':'EXG8'})
+
+# Reset reference channels type from eeg to eog
+raw.set_channel_types({'left': 'eog', 'right': 'eog', 'above': 'eog', 'below': 'eog'})
+print(raw.info)
+
+# Load data
+raw.pick(['eeg', 'eog', 'stim']).load_data()
+
+#Rereference to earlobes/mastoids (average of left and right)
+#Check eeg config file the name for mastoids, and then rename them
+raw.set_eeg_reference(ref_channels=['left-ref', 'right-ref'])
+
+#What is our baseline?
+# Baseline = (-0.099609375, 0)
+
+# Is it okay we use the common location map? We have different placement of P5 and P6.
+biosemi64_montage = mne.channels.make_standard_montage(kind = 'biosemi64')
+EEG_64channels = biosemi64_montage.ch_names
+eog_channels = ['left', 'right', 'above', 'below']
+# biosemi64_montage.plot()
+raw.set_montage(biosemi64_montage, on_missing = 'ignore')
+events = mne.find_events(raw)
+
+# Define graphs parameters
+plot_kwargs = dict(ylim=dict(eeg=(-15, 15), eog=(-15, 15), csd = (-15,15)))
+
+
+####################################################################################################################
+# Filters:
+#     Butterworth Zero Phase Filters
+#     Low Cutoff: 0.1 Hz, Time constant 1.5915s, 12 dB/oct ???????????
+#     High Cutoff: 30.0 Hz, 24 dB/oct ????????????
+#     Notch Filter: 50 Hz and 60 Hz
+raw.filter(l_freq=0.1, h_freq=30, method='iir', iir_params=None,picks = 'all')
+raw.filter(l_freq=0.1, h_freq=30, method='fir', phase = 'zero', picks = 'all')
+# raw.filter(l_freq=0.1, h_freq=30, method='fir', phase = 'zero', filter_length = '1s', picks = 'all')
+# alueError: The requested filter length 1025 is too short for the requested 0.05 Hz transition band, which requires 33793 samples
+raw.notch_filter(freqs = (50, 60), picks = 'all')
+# plot_kwargs = dict(ylim=dict(eeg=(-10, 10), eog=(-5, 15)))
+# raw.plot_psd(fmax = 35)
+####################################################################################################################
+
+
+####################################################################################################################
+# artifact removal
+    # Minimal allowed amplitude: -250.00 µV, Max: 250.00 µV
+    # MNE can reject epochs based on maximum peak-to-peak signal amplitude (PTP),
+    # i.e. the absolute difference between the lowest and the highest signal value.
+    # How can I remove signals based on singel peak but not differences between peaks?
+
+    # Annotating continuous data
+    # onsets = events[:, 0] / raw.info['sfreq'] - 0.5
+    # durations = [1] * len(events)
+    # descriptions = ['bad_event'] * len(events)
+    # event_annotation = mne.Annotations(onsets, durations, descriptions, orig_time=raw.info['meas_date'])
+    # raw.set_annotations(event_annotation)
+####################################################################################################################
+
+
+####################################################################################################################
+# Segmentation
+# o Segment size and position relative to reference markers:
+# o Start: -100 ms ????, End: 500.00 ms?????, Length: 600 ms?????
+# How long the tmin and tmax should be? Images are contiguous to the gray screen
+reject_criteria = dict(eeg=50e-6)
+flat_criteria = dict(eeg = 50e-8)
+events_len = len(events)
+CurIndex_event = 0
+converted_events = []
+for i in range(0,events_len):
+    converted_events.append(events[i, :])
+    if events[i, 2] > 250:
+        converted_events[i][2] = 5000 + events[i, 2]
+        continue
+    elif 1 <= events[i, 2] <= 250:
+        CurImage = Sub_Images_list[CurIndex_event]
+        if CurImage.startswith('Stimuli\\Targets\\'):
+            CurImage = CurImage[16:]
+        elif CurImage.startswith('Stimuli\\'):
+            CurImage = CurImage[8:]
+        converted_events[i][2] = All_Images_dict[CurImage]
+        CurIndex_event += 1
+converted_events = np.array(converted_events)
+stimuli_list = [str(i) for i in list(range(1,4708))]
+
+#EEG & EOG plots (without reject criteria)
+epochs_NoRestriction = mne.Epochs(raw, converted_events, tmin=-0.1, tmax=0.4, picks = EEG_64channels+eog_channels)
+epochs_NoRestriction.load_data()
+stimuli_epochs_NoRestriction = epochs_NoRestriction[stimuli_list]
+stimuli_epochs_NoRestriction.load_data()
+stimuli_epochs_plot_before_NoRestriction = stimuli_epochs_NoRestriction.average('eeg').plot(**plot_kwargs)
+stimuli_epochs_plot_before_NoRestriction.set_size_inches(27, 16)
+stimuli_epochs_plot_before_NoRestriction.savefig(os.path.join(SubMatrix_path,subject_name + "_stimuliEEG_epochs_before(All events).png"))
+stimuli_epochs_plot_before_NoRestriction = stimuli_epochs_NoRestriction.average('eog').plot(**plot_kwargs)
+stimuli_epochs_plot_before_NoRestriction.set_size_inches(27, 16)
+stimuli_epochs_plot_before_NoRestriction.savefig(os.path.join(SubMatrix_path,subject_name + "_stimuliEOG_epochs_before(All events).png"))
+
+#EEG & EOG plots (Use  AutoReject criteria)
+AutoReject = get_rejection_threshold(epochs_NoRestriction)
+epochs_autoreject = mne.Epochs(raw, converted_events, tmin=-0.1, tmax=0.4, reject = AutoReject, picks = EEG_64channels+eog_channels)
+stimuli_epochs_AutoReject = epochs_autoreject[stimuli_list]
+stimuli_epochs_AutoReject.load_data()
+stimuli_epochs_plot_before_AutoReject = stimuli_epochs_AutoReject.average('eeg').plot(**plot_kwargs)
+stimuli_epochs_plot_before_AutoReject.set_size_inches(27, 16)
+stimuli_epochs_plot_before_AutoReject.savefig(os.path.join(SubMatrix_path,subject_name + "_stimuliEEG_epochs_before(AutoReject).png"))
+stimuli_epochs_plot_before_AutoReject = stimuli_epochs_AutoReject.average('eog').plot(**plot_kwargs)
+stimuli_epochs_plot_before_AutoReject.set_size_inches(27, 16)
+stimuli_epochs_plot_before_AutoReject.savefig(os.path.join(SubMatrix_path,subject_name + "_stimuliEOG_epochs_before(AutoReject).png"))
+
+#EEG & EOG plots (Use self-defined reject criteria)
+epochs = mne.Epochs(raw, converted_events, tmin=-0.1, tmax=0.4, reject = reject_criteria, flat = flat_criteria, picks = EEG_64channels+eog_channels)
+epochs.load_data()
+stimuli_epochs = epochs[stimuli_list]
+stimuli_epochs.load_data()
+stimuli_epochs_plot_before = stimuli_epochs.average('eeg').plot(**plot_kwargs)
+stimuli_epochs_plot_before.set_size_inches(27, 16)
+stimuli_epochs_plot_before.savefig(os.path.join(SubMatrix_path,subject_name + "_stimuli_epochsEEG_before(with reject criteria).png"))
+stimuli_epochs_plot_before = stimuli_epochs.average('eog').plot(**plot_kwargs)
+stimuli_epochs_plot_before.set_size_inches(27, 16)
+stimuli_epochs_plot_before.savefig(os.path.join(SubMatrix_path,subject_name + "_stimuli_epochsEOG_before(with reject criteria).png"))
+####################################################################################################################
+
+
+####################################################################################################################
+# Ocular correction (Gratton & Coles)
+model_plain = EOGRegression(picks='eeg', picks_artifact='eog').fit(stimuli_epochs)
+stimuli_epochs_clean_plain = model_plain.apply(stimuli_epochs)
+stimuli_epochs_clean_plain.apply_baseline()
+stimuli_epochs_clean_plain.save(os.path.join(SubMatrix_path, subject_name + "-OC-epo.fif"), overwrite = False)
+stimuli_epochs_eogout_plot = stimuli_epochs_clean_plain.average().plot(**plot_kwargs)
+stimuli_epochs_eogout_plot.set_size_inches(27, 16)
+stimuli_epochs_eogout_plot.savefig(os.path.join(SubMatrix_path,subject_name + "_stimuli_epochs_after_OC(with reject criteria).png"))
+
+model_plain_AutoReject = EOGRegression(picks='eeg', picks_artifact='eog').fit(stimuli_epochs_AutoReject)
+stimuli_epochs_clean_plain_AutoReject = model_plain_AutoReject.apply(stimuli_epochs_AutoReject)
+stimuli_epochs_clean_plain_AutoReject.apply_baseline()
+stimuli_epochs_clean_plain_AutoReject.save(os.path.join(SubMatrix_path, subject_name + "-OC-AutoReject-epo.fif"), overwrite = False)
+stimuli_epochs_eogout_plot_AutoReject = stimuli_epochs_clean_plain_AutoReject.average().plot(**plot_kwargs)
+stimuli_epochs_eogout_plot_AutoReject.set_size_inches(27, 16)
+stimuli_epochs_eogout_plot_AutoReject.savefig(os.path.join(SubMatrix_path,subject_name + "_stimuli_epochs_after_OC(AutoReject).png"))
+
+model_plain_NoRestriction = EOGRegression(picks='eeg', picks_artifact='eog').fit(stimuli_epochs_NoRestriction)
+stimuli_epochs_clean_plain_NoRestriction = model_plain_NoRestriction.apply(stimuli_epochs_NoRestriction)
+stimuli_epochs_clean_plain_NoRestriction.apply_baseline()
+stimuli_epochs_clean_plain_NoRestriction.save(os.path.join(SubMatrix_path, subject_name + "-OC-NoRestriction-epo.fif"), overwrite = False)
+stimuli_epochs_eogout_plot_NoRestriction= stimuli_epochs_clean_plain_NoRestriction.average().plot(**plot_kwargs)
+stimuli_epochs_eogout_plot_NoRestriction.set_size_inches(27, 16)
+stimuli_epochs_eogout_plot_NoRestriction.savefig(os.path.join(SubMatrix_path,subject_name + "_stimuli_epochs_after_OC(All events).png"))
+####################################################################################################################
+
+
+####################################################################################################################
+# CSD transformation
+#     o Order of splines: 4
+#     o Maximal degree of Legendre polynomials: 10
+#     o Approximation parameter Lambda: 1.000000e-005
+stimuli_epochs_csd = mne.preprocessing.compute_current_source_density(stimuli_epochs_clean_plain, lambda2=1e-5, stiffness=4, n_legendre_terms=10)
+stimuli_epochs_csd.save(os.path.join(SubMatrix_path,subject_name + "-OC&CSD-epo.fif"), overwrite = False)
+evoked_plot = stimuli_epochs_csd.average().plot(**plot_kwargs)
+evoked_plot.set_size_inches(27, 16)
+evoked_plot.savefig(os.path.join(SubMatrix_path,subject_name + "_stimuli_epochs_after_OC&CSD(with reject criteria).png"))
+
+stimuli_epochs_csd_AutoReject = mne.preprocessing.compute_current_source_density(stimuli_epochs_clean_plain_AutoReject, lambda2=1e-5, stiffness=4, n_legendre_terms=10)
+stimuli_epochs_csd_AutoReject.save(os.path.join(SubMatrix_path,subject_name + "-OC&CSD-AutoReject-epo.fif"), overwrite = False)
+evoked_plot_AutoReject = stimuli_epochs_csd_AutoReject.average().plot(**plot_kwargs)
+evoked_plot_AutoReject.set_size_inches(27, 16)
+evoked_plot_AutoReject.savefig(os.path.join(SubMatrix_path,subject_name + "_stimuli_epochs_after_OC&CSD(AutoReject).png"))
+
+stimuli_epochs_csd_NoRestriction = mne.preprocessing.compute_current_source_density(stimuli_epochs_clean_plain_NoRestriction, lambda2=1e-5, stiffness=4, n_legendre_terms=10)
+stimuli_epochs_csd_NoRestriction.save(os.path.join(SubMatrix_path,subject_name + "-OC&CSD-NoRestriction-epo.fif"), overwrite = False)
+evoked_plot_NoRestriction = stimuli_epochs_csd_NoRestriction.average().plot(**plot_kwargs)
+evoked_plot_NoRestriction.set_size_inches(27, 16)
+evoked_plot_NoRestriction.savefig(os.path.join(SubMatrix_path,subject_name + "_stimuli_epochs_after_OC&CSD(All events).png"))
+####################################################################################################################
+
+
+
+####################################################################################################################
+
+
---
+++ Analysis/eeg_mutiple_regression.py
@@ -0,0 +1,245 @@
+import dill
+import numpy as np
+import mne
+import pandas as pd
+import os
+import yaml
+from sklearn import linear_model
+import matplotlib.pyplot as plt
+import scipy.stats as stats
+
+# Enter subject number
+subject_name = input("Enther the subject number (in form of sub_x): ")
+
+# Define minimum repetition
+min_rep = 5
+# Set the plot size
+plt.rcParams['figure.figsize'] = [18, 12]
+
+project_path = r"C:\Users\15202\OneDrive\C_\University of Amsterdam\Intern\CAVA_project"
+server_path = r"Y:\Projects\2023_Scholte_FMG1441"
+epoch_path = os.path.join(server_path, "Data", subject_name, "Preprocessed epochs", subject_name + "-OC&CSD-AutoReject-epo.fif")
+dic_path = os.path.join(project_path, "EventsID_Dictionary.csv")
+statistics_path = os.path.join(project_path, "Stimuli")
+# Read the Image dictionary
+All_Images_df = pd.read_csv(dic_path, header=None)
+All_Images_dict = dict(zip(All_Images_df[1], All_Images_df[0]))
+
+# Get CE & SC list
+with open(os.path.join(statistics_path, "lgn_statistics.pkl"), 'rb') as f:
+    result = dill.load(f)
+statistics_filename = result['filenames']
+# The first dim: number of images, second: the RGB, third: number of random crops, fourth:  center vs. periphery
+ce = result['CE']
+ce_list = ce[:, :, 0, 0]
+ce_list = ce_list.mean(axis=1)  # Average across RGB channels
+sc = result['SC']
+sc_list = sc[:, :, 0, 0]
+sc_list = sc_list.mean(axis=1)
+
+# Get Epochs
+sub_epochs = mne.read_epochs(epoch_path)
+sub_epochs = sub_epochs.pick_types(csd = True)
+channel_names = sub_epochs.ch_names
+# Get events filename
+events = sub_epochs.events[:, 2]
+events_filename = np.array([All_Images_dict[i] for i in events])
+# oz_epochs = sub_epochs.pick_channels(ch_names=['Oz'])
+
+
+# Get the subject unique images list
+with open(os.path.join(statistics_path, "eeg_oads_stimulus_filenames.yml"), 'rb') as f:
+    subjects = yaml.load(f, Loader=yaml.UnsafeLoader)
+subjects = subjects[subject_name]
+sub_sti = [i.split('\\')[1] for i in subjects if len(i) < 37]   # Stimuli images
+sub_sti = np.array(sub_sti)
+targets = [i.split('\\')[2] for i in subjects if len(i) >= 37]  # Target images
+targets = np.array(targets)
+
+# Get index of images in the events
+Training_images_index = dict()
+Testing_images_index = dict()
+missing_images_counter = 0
+for i in range(len(sub_sti)):
+    cur_image_index = np.where(events_filename == sub_sti[i])[0]
+    # cur_image_index = [a for a, b in enumerate(events_filename) if b == sub_sti[i]]
+    if len(cur_image_index) > min_rep:
+        Testing_images_index[events_filename[cur_image_index[0]]] = cur_image_index
+    elif 0 < len(cur_image_index) <= min_rep:
+        Training_images_index[events_filename[cur_image_index[0]]] = cur_image_index
+    else:
+        print("Didn't find ", sub_sti[i], " in the events\n")
+        missing_images_counter += 1
+if missing_images_counter == 0:
+    print("NO missing images!\n")
+
+# Use the index to make the design matrix
+x = []    # Design matrix
+eeg_index = []
+repetition_counter = 0
+group_index = [0]
+cur_image = 0
+missing_statistics_counter = 0
+Training_images_index_keys = list(Training_images_index.keys())
+for i in range(len(events)):
+    if repetition_counter >= min_rep:
+        break
+    cur_CE_para = [0] * min_rep  # Initiate CE parameters list for current image
+    cur_SC_para = [0] * min_rep  # Initiate SC parameters list for current image
+    while len(Training_images_index[Training_images_index_keys[cur_image]]) <= repetition_counter:
+        if cur_image == len(Training_images_index_keys) - 1:
+            group_index.append(i-missing_statistics_counter)
+            cur_image = 0
+            repetition_counter += 1
+        else:
+            cur_image += 1
+        if repetition_counter >= min_rep:
+            break
+    if repetition_counter >= min_rep:
+        break
+    cur_image_name = Training_images_index_keys[cur_image]
+    if cur_image_name not in statistics_filename:
+        print(missing_statistics_counter, ": " "SC and CE for", cur_image_name, "are missing!\n")
+        missing_statistics_counter += 1
+    else:
+        eeg_index.append(Training_images_index[cur_image_name][repetition_counter])
+        statistics_index = statistics_filename.index(cur_image_name)  # Get the corresponding SC and CE index
+        cur_CE_para[repetition_counter] = ce_list[statistics_index]
+        cur_SC_para[repetition_counter] = sc_list[statistics_index]
+        x.append(cur_CE_para + cur_SC_para)
+    if cur_image == len(Training_images_index_keys) - 1:
+        group_index.append(i-missing_statistics_counter)
+        cur_image = 0
+        repetition_counter += 1
+    else:
+        cur_image += 1
+if missing_statistics_counter == 0:
+    print("NO missing statistics!\n")
+# Z-score each set of SC and CE in x
+x = np.array(x)
+t = np.array(x)
+for i in range(repetition_counter):
+    x[group_index[i]:group_index[i+1], i] = stats.zscore(x[group_index[i]:group_index[i+1], i])
+    x[group_index[i]:group_index[i+1], i + min_rep] = stats.zscore(x[group_index[i]:group_index[i+1], i + min_rep])
+# plt.imshow(x)
+# plt.show()
+
+# Fit regression model for each channel
+r_sq_list = []
+coef_list = []
+for i in range(len(channel_names)):
+    # Get the corresponding EEG data
+    cur_channel = channel_names[i]
+    channel_data = sub_epochs._data[eeg_index, i, :]
+    cur_r_sq = []
+    cur_coef = []
+    # Fit linear regression model for each time points
+    for j in range(channel_data.shape[1]):
+        y = stats.zscore(np.array(channel_data[:, j]))
+        regr = linear_model.LinearRegression().fit(x, y)
+        # Save the Coefficients
+        cur_coef.append(list(regr.coef_))
+        cur_r_sq.append(regr.score(x, y))
+    # plot the R^2 for current channel
+    plt.figure(1)
+    plt.plot(sub_epochs.times, cur_r_sq, label = cur_channel)
+    r_sq_list. append(cur_r_sq)
+    # plot CEs for current channel
+    plt.figure(2)
+    plt.plot(sub_epochs.times, np.array(cur_coef)[:, 0:min_rep].mean(axis = 1), label = cur_channel)
+    # plot SCs for current channel
+    plt.figure(3)
+    plt.plot(sub_epochs.times, np.array(cur_coef)[:, min_rep:].mean(axis = 1), label = cur_channel)
+    coef_list.append(cur_coef)
+    del cur_r_sq, cur_coef
+r_sq_list = np.array(r_sq_list)
+coef_list = np.array(coef_list)
+
+# plot the average
+# mean_r_sq = r_sq_list.mean(axis = 0)
+# plt.plot(sub_epochs.times, mean_r_sq, 'k', label = 'mean', linewidth=3)
+
+# Setting parameters for the R^2 plot
+plt.figure(1)
+plt.xlabel('Time')
+plt.ylabel('R^2')
+plt.title(subject_name + "R^2")
+# plt.legend(fontsize="20")
+plt.savefig(os.path.join(project_path, subject_name + "_R^2.png"))
+# plt.show()
+# Save r_sq
+np.save(os.path.join(project_path, subject_name + "_Rsq.npy"), r_sq_list)
+
+# Setting parameters for the CE and SC plot
+plt.figure(2)
+plt.xlabel('Time')
+plt.ylabel('CE')
+plt.title(subject_name + "β-coefficients (CE)")
+plt.legend(fontsize="20")
+plt.savefig(os.path.join(project_path, subject_name + "_CE.png"))
+# plt.show()
+
+plt.figure(3)
+plt.xlabel('Time')
+plt.ylabel('SC')
+plt.title(subject_name + "β-coefficients (SC)")
+plt.legend(fontsize="20")
+plt.savefig(os.path.join(project_path, subject_name + "_SC.png"))
+# plt.show()
+# Save coefficients
+np.save(os.path.join(project_path, subject_name + "_βcoef.npy"), coef_list)
+
+
+# Plot Oz
+Oz_index = np.where(np.array(channel_names) == 'Oz')[0]
+Oz_data = sub_epochs._data[eeg_index, Oz_index, :]
+cur_r_sq = []
+cur_coef = []
+# Fit linear regression model for each time points
+for j in range(Oz_data.shape[1]):
+    y = stats.zscore(np.array(Oz_data[:, j]))
+    regr = linear_model.LinearRegression().fit(x, y)
+    # Save the Coefficients
+    cur_coef.append(list(regr.coef_))
+    cur_r_sq.append(regr.score(x, y))
+cur_r_sq = np.array(cur_r_sq)
+# plot the Oz R^2
+plt.figure(4)
+plt.plot(sub_epochs.times, cur_r_sq)
+plt.xlabel('Time')
+plt.ylabel('R^2')
+plt.title(subject_name + "R^2 (Oz)")
+plt.savefig(os.path.join(project_path, subject_name + "_R^2(Oz).png"))
+# Save Oz r_sq
+np.save(os.path.join(project_path, subject_name + "_Rsq(Oz).npy"), cur_r_sq)
+
+# plot CEs for channel Oz
+plt.figure(5)
+plt.plot(sub_epochs.times, np.array(cur_coef)[:, 0:min_rep].mean(axis = 1), label = "CE")
+# plot SCs for current channel
+plt.plot(sub_epochs.times, np.array(cur_coef)[:, min_rep:].mean(axis = 1), label = "SC")
+plt.xlabel('Time')
+plt.ylabel('β-coefficients')
+plt.title(subject_name + "β-coefficients (Oz)")
+plt.legend(fontsize="20")
+plt.savefig(os.path.join(project_path, subject_name + "_β-coefficients(Oz).png"))
+# Save coefficients
+np.save(os.path.join(project_path, subject_name + "_βcoef(Oz).npy"), cur_coef)
+
+# Plot the design matrix
+plt.figure(6)
+# Add intercepts column
+Intercept_constant = np.ones((x.shape[0],1))
+x = np.hstack((Intercept_constant,x))
+# # Save Design matrix
+np.save(os.path.join(project_path, subject_name + "_Design_Matrix.npy"), x)
+new_x = np.ma.masked_where(x == 0, x)
+cmap = plt.get_cmap('inferno')
+cmap.set_bad(color = 'white')
+plt.pcolormesh(new_x, cmap = cmap)
+plt.xlabel('Parameters')
+plt.ylabel('Training images')
+plt.title(subject_name + "Design Matrix")
+plt.colorbar()
+# plt.legend(fontsize="20")
+plt.savefig(os.path.join(project_path, subject_name + "_Design Matrix.png"))
